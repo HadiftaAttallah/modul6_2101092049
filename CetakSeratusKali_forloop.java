8ef5d08345dc632beec7e197aff634d01ff04929
HadiftaAttallah <114393928+HadiftaAttallah@users.noreply.github.com>
2022-11-16 22:27:01 +0700

    Add files via upload

---
+++ CetakSeratusKali_forloop.java
@@ -0,0 +1,9 @@
+package HadiftaAttallahPutri_2101092049_modul6;
+
+public class CetakSeratusKali_forloop {
+ public static void main(String[] args)
+ {int i;
+    for (i=1;i<=100;i++)
+    {System.out.println(i+ "Hadifta Attallah Putri");}
+ }   
+}
